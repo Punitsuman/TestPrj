8551123a34cc6c18cf320e141b449ac7ae9e613b
Punit Suman <punitsuman89@gmail.com>
2019-03-05 05:24:57 +0530

    Added varags

--- PS/src/inheritanceps/InheritanceTester.java
+++ PS/src/inheritanceps/InheritanceTester.java
@@ -9,12 +9,22 @@ public class InheritanceTester {
 		Child c = new Child();
 		c.setGid(10);
 		System.out.println(c.getGid());
-		
+		System.out.println(c.getGname());
 		c.setGname("Punit");
 		
 		System.out.println(c.gname.equals(new String("Punit")));
 		
+		InheritanceTester it = new InheritanceTester();
+		it.varags(1,2,3,4,5);
+		it.varags();
 
+	}
+	
+	public void varags(int... nos){
+		System.out.println(nos.length);
+		for(int i:nos)
+			System.out.println(i);
+		
 	}
 
 }
