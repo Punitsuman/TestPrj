bf2df0dcd4d01ea568b9973ecf26a7f772c21243
Punit Suman <punitsuman89@gmail.com>
2019-03-07 21:35:54 +0530

    Refactored code.

--- PS/src/packageps/ClassInPS2.java
+++ PS/src/packageps/ClassInPS2.java
@@ -1,16 +1,32 @@
 package packageps;
 
+import java.io.IOException;
+
 import packagep.SuperClassPS;
 import packagep.SuperClassPS;
 
 
-public class ClassInPS2 {
+public final class ClassInPS2 {
 	
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		 SuperClassPS superclassps = new SuperClassPS();
 		/* T1 t1 = new T1();
 		 T2 t2 = new T2();*/
+		 
+		 ClassInPS2 c = new ClassInPS2();
+		 
+		 
+		 System.out.print(1);
+		 
+		 System.out.println("Enter any integer");
+		 
+		int i = System.in.read();
+		
+		System.out.println("i : "+ (char)i);
+		 
+		 
+		 
 	}
 
 	
